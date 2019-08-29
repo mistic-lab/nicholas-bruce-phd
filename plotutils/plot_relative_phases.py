954818d7204d34f428fc464a256373e9e34a2c41
Nicholas Bruce <saltair93+github@gmail.com>
2019-08-29 14:12:10 -0700

    added stable colorbar and option for unwrapping

--- plotutils/plot_relative_phases.py
+++ plotutils/plot_relative_phases.py
@@ -13,6 +13,8 @@ parser = argparse.ArgumentParser(
 #                     help='input h5 file of time series')
 parser.add_argument("--antenna", type=int, default=240,
                     help='antenna index to use')
+parser.add_argument("--unwrap", type=bool, default=False,
+                    help="whether to unwrap the phase or not")
 args = parser.parse_args()
 
 
@@ -34,6 +36,8 @@ with h5py.File('058628_001748255_ACM.hdf5','r') as f:
                 print("Averaging over a nan at ({},{})".format(i,j))
                 arr[i,j] = (arr[i,j+1]+arr[i,j-1])/2
 
+    phaseMax = np.max(arr)
+    phaseMin = np.min(arr)
 
     for i in range(arr.shape[1]):
         fig, ax = plt.subplots()
@@ -41,7 +45,8 @@ with h5py.File('058628_001748255_ACM.hdf5','r') as f:
         ax.plot(coords[0], coords[1], 'ko', ms=3)
         ax.plot(coords[0, args.antenna], coords[1, args.antenna], 'ro', ms=8 )
         ax.set_title("{} UTC".format(datetime.utcfromtimestamp(f['times'][i])))
-        cbar = ax.tricontourf(coords[0], coords[1], arr[:,i])
+        cbar = ax.tricontourf(coords[0], coords[1], arr[:,i], np.arange(phaseMin,phaseMax, (phaseMax-phaseMin)/50),extend='both')
+        # cbar.set_clim(vmin=phaseMin, vmax=phaseMax)
         fig.colorbar(cbar, label='Phase (rads)')
         ax.set_xlabel("X distance from array center (m)")
         ax.set_ylabel("Y distance from array center (m)")
