ed0f0ce5c41394a9b6f3629cfc8ce02a01fbb037
Nicholas Bruce <nsbruce@cedar1.cedar.computecanada.ca>
2019-09-18 16:18:14 -0700

    Updated incorrect path to builddatasets.py

--- compute-canada-scripts/buildAEdatasets.sh
+++ compute-canada-scripts/buildAEdatasets.sh
@@ -9,6 +9,6 @@ module load python/3.7.4
 module load scipy-stack/2019a
 
 source /home/nsbruce/projects/def-peterdri/nsbruce/RFI/venv/bin/activate
-python /home/nsbruce/projects/def-peterdri/nsbruce/RFI/autoencoder-anomaly-detector/build_datasets.py
+python /home/nsbruce/projects/def-peterdri/nsbruce/RFI/autoencoder-anomaly-detector/code/build_datasets.py
 deactivate
 
