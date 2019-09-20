7704f6dba328138ae114d1764eb48e40258e7bba
Nicholas Bruce <nsbruce@uvic.ca>
2019-09-20 06:58:57 -0700

    Updated runAE on CC (ran out of time & adjusted resources)

--- compute-canada-scripts/runAE.sh
+++ compute-canada-scripts/runAE.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 #SBATCH --account=def-peterdri
-#SBATCH --time=10:00:00
+#SBATCH --time=22:00:00
 #SBATCH --job-name=run-anomaly-autoencoder
-#SBATCH --mem=240G
-#SBATCH --cpus-per-task=24
+#SBATCH --mem=10G
+#SBATCH --cpus-per-task=48
 
 module load python/3.7.4
 module load scipy-stack/2019a
 
 source /home/nsbruce/projects/def-peterdri/nsbruce/RFI/venv/bin/activate
-python /home/nsbruce/projects/def-peterdri/nsbruce/RFI/autoencoder-anomaly-detector/main.py
+python /home/nsbruce/projects/def-peterdri/nsbruce/RFI/autoencoder-anomaly-detector/code/main.py
 deactivate
 
