0277ebc28330cf2696348bba03ea9afa5fae6bf3
Nicholas Bruce <saltair93+github@gmail.com>
2019-09-21 07:24:50 -0700

    Updated runAE to use GPUs

--- compute-canada-scripts/runAE.sh
+++ compute-canada-scripts/runAE.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
 #SBATCH --account=def-peterdri
 #SBATCH --time=22:00:00
-#SBATCH --job-name=run-anomaly-autoencoder
+#SBATCH --job-name=run-anomaly-autoencoder-gpu
 #SBATCH --mem=10G
-#SBATCH --cpus-per-task=48
+#SBATCH --cpus-per-task=24
+#SBATCH --gres=gpu:4
 
 module load python/3.7.4
 module load scipy-stack/2019a
+module load cuda
 
 source /home/nsbruce/projects/def-peterdri/nsbruce/RFI/venv/bin/activate
 python /home/nsbruce/projects/def-peterdri/nsbruce/RFI/autoencoder-anomaly-detector/code/main.py
