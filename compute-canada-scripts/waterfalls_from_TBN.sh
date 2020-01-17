83f8cc3dccd18dea2489e11da33ddfc1ab7958f9
Nicholas Bruce <saltair93+github@gmail.com>
2020-01-17 13:43:05 -0800

    auto-waterfall

---
+++ compute-canada-scripts/waterfalls_from_TBN.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+#SBATCH --account=def-peterdri
+#SBATCH --time=01:00:00
+#SBATCH --job-name=waterfall-builder
+#SBATCH --mem=20G
+#SBATCH --cpus-per-task=8
+
+module load gcc/5.4.0
+
+module load fftw/3.3.6
+
+module load scipy-stack/2019a
+
+export ATLAS=/project/def-peterdri/software/atlas-3.10.2
+
+export ATLAS_HOME=/project/def-peterdri/software/atlas-3.10.2
+
+source /home/nsbruce/projects/def-peterdri/software/lwa-1.2.4/bin/activate
+
+TBN_DIR=/home/nsbruce/projects/def-peterdri/LWA_Data/2019-12-29/RAW_TBN/
+NUM_SEC=240
+
+python /home/nsbruce/UViip/lwa-tools/code/plot_waterfallsTBN.py $TBN_DIR -s $NUM_SEC
+
+deactivate
+
